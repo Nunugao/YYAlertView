987a038acc0272734fe10fe32e5902a3dddf86e0
manager <manager@eden.local>
2016-11-16 14:25:15 +0800

    Initial Commit

---
+++ YYAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYAlertView
+//
+//  Created by manager on 16/11/16.
+//  Copyright © 2016年 Rockontrol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYAlertView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YYAlertView
+//
+//  Created by manager on 16/11/16.
+//  Copyright © 2016年 Rockontrol. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
